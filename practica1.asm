e5289d9b914681e19c144e2198ef5cdb5f47327a
Alberto <Alberto@MacBook-Pro-de-Alberto-Amigo-Alonso.local>
2014-10-16 21:40:46 +0200

    Error solucionado y comentarios

--- practica1.asm
+++ practica1.asm
@@ -1,32 +1,49 @@
+;
+; Código desarrollado por Sergio Delgado y Alberto Amigo.
+; Práctica 1 de AOC (IA32).
+;
+
 segment .data
-	prompt1 db "Introduzca el capital: ", 0
-	l_pro1 equ $-prompt1
+	; Etiqueta el mensaje que pide el capital y guarda su número de caracteres.
+	promptCap db "Introduzca el capital: ", 0
+	l_proCap equ $-promptCap
 
-	prompt2 db "Introduzca el redito: ", 0
-	l_pro2 equ $-prompt2
+	; Etiqueta el mensaje que pide el redito y guarda su número de caracteres.
+	promptRed db "Introduzca el redito: ", 0
+	l_proRed equ $-promptRed
 
-	prompt3 db "Introduzca el tiempo: ", 0
-	l_pro3 equ $-prompt3
+	; Etiqueta el mensaje que pide el tiempo y guarda su número de caracteres.
+	promptTie db "Introduzca el tiempo: ", 0
+	l_proTie equ $-promptTie
 
 segment .bss
+	; Reserva espacio de 32 bits para almacenar el capital
 	capital resd 1	
+
+	; Reserva espacio de 32 bits para almacenar el redito
 	redito resd 1
+
+	; Reserva espacio de 32 bits para almacenar el tiempo
 	tiempo resd 1
-	resultado resd 1
+
+	; Reserva espacio de 32 bits para almacenar el intercambio
+	input resd 1
 	
 segment .text
 	global _start
 
 	_start:
+		; Imprimir por pantalla la petición del capital
 		mov eax, 4
 		mov ebx, 1
-		mov ecx, prompt1
-		mov edx, l_pro1
+		mov ecx, promptCap
+		mov edx, l_proCap
 		int 80h
 
+		; Recoger de teclado el valor introducido para capital
 		mov eax, 3
 		mov ebx, 0
-		mov ecx, capital
+		mov ecx, input
 		mov edx, 80
 		int 80h
 
@@ -35,13 +52,21 @@ segment .text
 
 		mov eax, 4
 		mov ebx, 1
-		mov ecx, prompt2
-		mov edx, l_pro2
+		mov ecx, capital
+		mov edx, 99
 		int 80h
 
+		; Imprimir por pantalla la petición del rédito
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, promptRed
+		mov edx, l_proRed
+		int 80h
+
+		; Recoger de teclado el valor introducido para redito
 		mov eax, 3
 		mov ebx, 0
-		mov ecx, redito
+		mov ecx, input
 		mov edx, 80
 		int 80h
 
@@ -50,37 +75,59 @@ segment .text
 
 		mov eax, 4
 		mov ebx, 1
-		mov ecx, prompt3
-		mov edx, l_pro3
+		mov ecx, redito
+		mov edx, 99
 		int 80h
 
+		; Imprimir por pantalla la petición del tiempo
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, promptTie
+		mov edx, l_proTie
+		int 80h
+
+		; Recoger de teclado el valor introducido para tiempo
 		mov eax, 3
 		mov ebx, 0
-		mov ecx, tiempo
+		mov ecx, input
 		mov edx, 80
 		int 80h
 
 		call ascii_bin
 		mov dword[tiempo], eax
 
+		mov eax, 4
+		mov ebx, 1
+		mov ecx, tiempo
+		mov edx, 99
+		int 80h
+
 		mov eax, 1
 		mov ebx, 0
 		int 80h
 
+	;
+	; Función para convertir de codificación ASCII a números.
+	; Parámetros:
+	; 	· eax - Cantidad de dígitos del número
+	;	· [input] - Número en ASCII a convertir
+	; Devuelve:
+	;	· eax - Número en binario
+	;
 	ascii_bin:
-		dec eax
-		mov ecx, eax
-		mov edx, 0
-		mov eax, 0
+		dec eax							;
+		mov ecx, eax					;
+		mov edx, 0 						;
+		mov eax, 0 						;
 
-		loop_start:
-			mov bl, byte[capital+edx]
-			sub bl, 0x30
-			imul eax, eax, 10
-			movzx ebx, bl
-			add eax, ebx
-			inc edx
-			loop loop_start
+		loop_start:						;
+			mov bl, byte[input+edx]		;
+			sub bl, 0x30				;
+			imul eax, eax, 10			;
+			movzx ebx, bl				;
+			add eax, ebx				;
+			inc edx						;
+			loop loop_start				;
 		ret
 		
 		
